a4a19bfc90f537cafe5f525efb03498217461f10
SirZen476 <danielzenkin82@gmail.com>
2024-08-28 14:18:55 +0300

    basic shapes and Field and screen init

--- Tetris_cmd/Tetris_cmd.cpp
+++ Tetris_cmd/Tetris_cmd.cpp
@@ -1,20 +1,52 @@
 // Tetris_cmd.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This is a project made to practice C++ with github.
 
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <windows.h>
 
+using namespace std;
+
+wstring tetris_shapes[7];// array to save tetris shapes
+// screen and field width and hight
+int screenWidth = 80;
+int screenHeight = 30;
+int FieldWidth = 12;
+int FieldHeight = 18;
+
+unsigned char* pField = nullptr; // pointer to elements of field in unsinged char array, that will be allocated dynamically
+ 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // append tetris shapes
+    tetris_shapes[0].append(L"..X...X...X...X.");
+    tetris_shapes[1].append(L"..X..XX...X.....");
+    tetris_shapes[2].append(L".....XX..XX.....");
+    tetris_shapes[3].append(L"..X..XX..X......");
+    tetris_shapes[4].append(L".X...XX...X.....");
+    tetris_shapes[5].append(L".X...X...XX.....");
+    tetris_shapes[6].append(L"..X...X..XX.....");
+
+    // calculate screen and Field size
+    int screenSize = screenWidth * screenHeight;
+    int FieldSize = FieldWidth * FieldHeight;
+    // Create screen Buffer
+    wchar_t* screen = new wchar_t[screenSize];
+    for (int i = 0; i < screenSize; i++) screen[i] = L' ';
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    //Creating play Field Buffer
+    pField = new unsigned char[FieldSize]; 
+    //Board Boundary setup 
+    for (int x = 0; x < FieldWidth;x++) {
+        for (int y = 0; y < FieldHeight;y++) {
+            pField[y * FieldWidth + x] = ( (x == 0) || (x == FieldWidth - 1) || (y == FieldHeight - 1)) ? 9 : 0;
+            // in this setup, boundry is 9 , empty is 0;
+        }
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
